6e024449b3da278b8c71e6402af6ec33fce45456
Trevor O'Dwyer <trevorodwyer619@hotmail.com>
2023-06-22 21:53:29 +0100

    Create gameover screen in scoreboard.py file

--- main.py
+++ main.py
@@ -32,6 +32,7 @@ while game_is_on:
     for car in car_manager.all_cars:
         if car.distance(player) < 20:
             game_is_on = False
+            scoreboard.game_over()
 
     # Detect if the player makes it to the other side
     if player.is_at_finish_line():
