cd3390904cb423d3b53f9e5dbe48911562085870
Trevor O'Dwyer <trevorodwyer619@hotmail.com>
2023-06-22 20:13:59 +0100

    Create screen + starter game code in main.py file

--- main.py
+++ main.py
@@ -1,3 +1,17 @@
 # Turtle Crossing Game
 # Game inspired by Frogger, created using the Turtle graphics library
 # 22/06/2023
+
+import time
+from turtle import Screen
+
+screen = Screen()
+screen.setup(width=600, height=600)
+screen.tracer(0)
+
+game_is_on = True
+while game_is_on:
+    time.sleep(0.1)
+    screen.update()
+
+screen.exitonclick()
